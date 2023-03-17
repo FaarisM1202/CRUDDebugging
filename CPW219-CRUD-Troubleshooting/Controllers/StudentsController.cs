81a343660ca40124c40b94ae46c1a104a9398a15
Faaris Mohamed <mohamed8876@students.cptc.edu>
2023-03-16 23:03:22 -0700

    Problems not solved.

--- CPW219-CRUD-Troubleshooting/Controllers/StudentsController.cs
+++ CPW219-CRUD-Troubleshooting/Controllers/StudentsController.cs
@@ -24,16 +24,18 @@ namespace CPW219_CRUD_Troubleshooting.Controllers
         {
             return View();
         }
-
+        // Same here:
+        // My data was not populating and I couldn't figure it out
+        // So, I added some test code into the database manually so
+        // you can test out the CRUD ability fully.
         [HttpPost]
         public IActionResult Create(Student p)
         {
             if (ModelState.IsValid)
             {
                 StudentDb.Add(p, context);
+                //context.SaveChanges();
                 TempData["Message"] = $"{p.Name} was added!";
-
-
                 return RedirectToAction("Index");
             }
 
@@ -62,6 +64,7 @@ namespace CPW219_CRUD_Troubleshooting.Controllers
             return View(p);
         }
 
+        // You'll need to refresh the page so that it disappears
         public IActionResult Delete(int id)
         {
             Student p = StudentDb.GetStudent(context, id);
